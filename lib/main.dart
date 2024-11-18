957e7f4aa1901e6a12e2f3b490b049220ee863b3
anasassad <lwalida1921@gmail.com>
2024-11-18 01:02:08 +0100

    Finalize the UI

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyAppState extends State<MyApp> {
   late double _numberFrom;
 
   String _startMeasure = 'meters';
+  String _convertedMeasure = 'grams';
 
   final List<String> _measures = [
     'meters',
@@ -35,6 +36,16 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle inputStyle = TextStyle(
+      fontSize: 20,
+      color: Colors.blueAccent,
+    );
+
+    final TextStyle labelStyle = TextStyle(
+      fontSize: 24,
+      color: Colors.blueGrey,
+    );
+
     return MaterialApp(
       title: 'Measures Converter',
       home: Scaffold(
@@ -42,35 +53,93 @@ class MyAppState extends State<MyApp> {
           title: const Text('Measures Converter'),
           backgroundColor: Colors.cyan,
         ),
-        body: Center(
+        body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
-          children: [
-            DropdownButton(
-                value: _startMeasure,
-                items: _measures.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _startMeasure = value!;
-                  });
-                }),
-            TextField(
-              onChanged: (text) {
-                var v = double.tryParse(text);
-                if (v != null) {
-                  setState(() {
-                    _numberFrom = v;
-                  });
-                }
-              },
-            ),
-            Text((_numberFrom == null) ? '' : _numberFrom.toString())
-          ],
-        )),
+              children: [
+                const Spacer(),
+                Text(
+                  'Value',
+                  style: labelStyle,
+                ),
+                const Spacer(),
+                TextField(
+                  style: inputStyle,
+                  decoration: const InputDecoration(
+                    hintText: 'Please insert the measure to be converted',
+                  ),
+                  onChanged: (text) {
+                    var v = double.tryParse(text);
+                    if (v != null) {
+                      setState(() {
+                        _numberFrom = v;
+                      });
+                    }
+                  },
+                ),
+                const Spacer(),
+                Text(
+                  'From',
+                  style: labelStyle,
+                ),
+                const Spacer(),
+                DropdownButton(
+                    isExpanded: true,
+                    value: _startMeasure,
+                    items: _measures.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    style: inputStyle,
+                    onChanged: (value) {
+                      setState(() {
+                        _startMeasure = value!;
+                      });
+                    }),
+                const Spacer(),
+                Text(
+                  'To',
+                  style: labelStyle,
+                ),
+                const Spacer(),
+                DropdownButton(
+                    isExpanded: true,
+                    value: _convertedMeasure,
+                    items: _measures.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    style: inputStyle,
+                    onChanged: (value) {
+                      setState(() {
+                        _convertedMeasure = value!;
+                      });
+                    }),
+                const Spacer(
+                  flex: 2,
+                ),
+                TextButton(
+                    onPressed: () => true,
+                    child: Text(
+                      'Convert',
+                      style: inputStyle,
+                    )),
+                const Spacer(
+                  flex: 2,
+                ),
+                Text(
+                  (_numberFrom == null) ? '' : _numberFrom.toString(),
+                  style: labelStyle,
+                ),
+                const Spacer(
+                  flex: 8,
+                ),
+              ],
+            )),
       ),
     );
   }
