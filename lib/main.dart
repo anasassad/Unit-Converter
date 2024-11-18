81882e7a0888f540f7af7a3a50067d8a1f4d247e
anasassad <lwalida1921@gmail.com>
2024-11-18 01:59:12 +0100

    Adding business logic to convert measures

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   late double _numberFrom;
+  late String _resultMessage;
 
   String _startMeasure = 'meters';
   String _convertedMeasure = 'grams';
@@ -28,20 +29,43 @@ class MyAppState extends State<MyApp> {
     'ounces'
   ];
 
+  final Map<String, int> _measuresMap = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds (lbs)': 6,
+    'ounces': 7,
+  };
+
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
+
   @override
   void initState() {
     _numberFrom = 0;
+    _resultMessage = '';
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final TextStyle inputStyle = TextStyle(
+    const TextStyle inputStyle = TextStyle(
       fontSize: 20,
       color: Colors.blueAccent,
     );
 
-    final TextStyle labelStyle = TextStyle(
+    const TextStyle labelStyle = TextStyle(
       fontSize: 24,
       color: Colors.blueGrey,
     );
@@ -58,7 +82,7 @@ class MyAppState extends State<MyApp> {
             child: Column(
               children: [
                 const Spacer(),
-                Text(
+                const Text(
                   'Value',
                   style: labelStyle,
                 ),
@@ -78,7 +102,7 @@ class MyAppState extends State<MyApp> {
                   },
                 ),
                 const Spacer(),
-                Text(
+                const Text(
                   'From',
                   style: labelStyle,
                 ),
@@ -99,7 +123,7 @@ class MyAppState extends State<MyApp> {
                       });
                     }),
                 const Spacer(),
-                Text(
+                const Text(
                   'To',
                   style: labelStyle,
                 ),
@@ -123,8 +147,16 @@ class MyAppState extends State<MyApp> {
                   flex: 2,
                 ),
                 TextButton(
-                    onPressed: () => true,
-                    child: Text(
+                    onPressed: () {
+                      if (_startMeasure.isEmpty ||
+                          _convertedMeasure.isEmpty ||
+                          _numberFrom == 0) {
+                        return;
+                      } else {
+                        convert(_numberFrom, _startMeasure, _convertedMeasure);
+                      }
+                    },
+                    child: const Text(
                       'Convert',
                       style: inputStyle,
                     )),
@@ -132,7 +164,7 @@ class MyAppState extends State<MyApp> {
                   flex: 2,
                 ),
                 Text(
-                  (_numberFrom == null) ? '' : _numberFrom.toString(),
+                  _resultMessage,
                   style: labelStyle,
                 ),
                 const Spacer(
@@ -142,5 +174,23 @@ class MyAppState extends State<MyApp> {
             )),
       ),
     );
+  }
+
+  void convert(double value, String from, String to) {
+    int? nFrom = _measuresMap[from];
+    int? nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+
+    if (result == 0) {
+      _resultMessage = 'This conversion cannot be performed';
+    } else {
+      _resultMessage =
+          '${_numberFrom.toString()} $_startMeasure are ${result.toString()} $_convertedMeasure';
+    }
+
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
   }
 }
